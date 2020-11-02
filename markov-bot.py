faa2396a52744434bd9d465a5e0b51b6a3e8e500
Siegmentation Fault <siegmentationfault@yandex.ru>
2020-11-02 16:41:23 +0700

    split

--- markov-bot.py
+++ markov-bot.py
@@ -1,7 +1,7 @@
-import socket
 import random
 import pickle
 import sys
+import irc
 
 # Ник, сервер, канал
 nickname = "markov"
@@ -17,67 +17,6 @@ size = (10, 30)
 
 # Разделитель слов для робота
 sep = ' '
-
-size     = lambda s: len(s.encode('utf-8'))
-nonempty = lambda xs: len(xs) > 0
-
-pop  = lambda xs: xs.pop(0)
-push = lambda xs, v: xs.insert(0, v)
-
-class IRC:
-    irc = socket.socket()
-  
-    def __init__(self):  
-        self.irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    def raw(self, msg):
-        print("+ " + msg)
-        self.irc.send("{}\n".format(msg).encode('utf-8'))
-
-    def prefix(self, chan):
-        return "PRIVMSG {} :".format(chan)
-
-    def split(self, msg, length):
-        buff, res, src = "", [], list(msg)
-
-        while nonempty(src):
-            while nonempty(src):
-                ch = pop(src)
-                if size(buff) + size(ch) <= length:
-                    buff += ch
-                else:
-                    push(src, ch)
-                    break
-
-            res.append(buff)
-            buff = ""
-
-        return res
-
-    def send(self, chan, msg):
-        prefix = self.prefix(chan)
-        for part in self.split(msg, 512 - len(prefix)):
-            self.raw(self.prefix(chan) + part)
-
-    def connect(self, server, channel, botnick):
-        print("Connecting to: " + server)
-        self.irc.connect((server, 6667))
-
-        self.raw("USER {nick} {nick} {nick} :Markov".format(nick=botnick))
-        self.raw("NICK {}".format(botnick))
-        self.raw("JOIN {}".format(channel))
-
-    def get(self):
-        try:
-            text = self.irc.recv(2040).decode('utf-8')
-
-            if text.find('PING') != -1:
-                server = text.split()[1]
-                self.raw("PONG {}".format(server))
-
-            return text
-        except UnicodeDecodeError:
-            return ""
 
 kv = {}
 with open(db, 'rb') as fin:
@@ -112,7 +51,7 @@ def gen(answer):
 
     return res
 
-irc = IRC()
+irc = irc.IRC()
 irc.connect(server, channel, nickname)
 
 while True:
